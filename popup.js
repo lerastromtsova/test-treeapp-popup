f8845457dfd528fcd7b795cf5977c4b9febba265
Valeria Stromtsova <v.stromtsova@yandex.ru>
2022-01-31 16:33:54 +0100

    fix call of function

--- popup.js
+++ popup.js
@@ -60,67 +60,67 @@
         popup.init()
       }
     })
-
-    let global = {
-
-      jq: null,
-      productId: '7556676026598',
-      variantId: '42106048872678',
-
-      init: function () {
-        this.eventListener();
-      },
-
-      loadScript: function (url, callback) {
-        let script = document.createElement("script");
-        script.type = "text/javascript";
-        if (script.readyState) {
-          script.onreadystatechange = function () {
-            if (script.readyState == "loaded" || script.readyState == "complete") {
-              script.onreadystatechange = null;
-              callback();
-            }
-          };
-        } else {
-          script.onload = function () {
-            callback();
-          };
-        }
-        script.src = url;
-        document.getElementsByTagName("head")[0].appendChild(script);
-      },
-
-      appendContent: function (content) {
-        this.jq(document.body).prepend(content);
-      },
-
-      addProductToCart: function (callback) {
-        let quantity = 1;
-        let data = {
-          items: [{
-            quantity: quantity,
-            id: global.variantId,
-            async: false
-          }]
-        };
-        this.jq.post('/cart/add.js', data, function(data) { console.log(data);}, "json");
-      },
-
-      eventListener: function () {
-        let self = this;
-        if ((typeof jQuery === 'undefined') || (parseFloat(jQuery.fn.jquery) < 1.7)) {
-          self.loadScript('//ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', function () {
-            var jQuery311 = jQuery.noConflict(true);
-            self.jq = jQuery311;
-            treeappPlugin(jQuery311);
-          });
-        } else {
-          self.jq = jQuery;
-          treeappPlugin(jQuery);
-        }
-      }
-    };
-
-    global.init();
   }
-}) ()
+
+  let global = {
+
+    jq: null,
+    productId: '7556676026598',
+    variantId: '42106048872678',
+
+    init: function () {
+      this.eventListener();
+    },
+
+    loadScript: function (url, callback) {
+      let script = document.createElement("script");
+      script.type = "text/javascript";
+      if (script.readyState) {
+        script.onreadystatechange = function () {
+          if (script.readyState == "loaded" || script.readyState == "complete") {
+            script.onreadystatechange = null;
+            callback();
+          }
+        };
+      } else {
+        script.onload = function () {
+          callback();
+        };
+      }
+      script.src = url;
+      document.getElementsByTagName("head")[0].appendChild(script);
+    },
+
+    appendContent: function (content) {
+      this.jq(document.body).prepend(content);
+    },
+
+    addProductToCart: function (callback) {
+      let quantity = 1;
+      let data = {
+        items: [{
+          quantity: quantity,
+          id: global.variantId,
+          async: false
+        }]
+      };
+      this.jq.post('/cart/add.js', data, function(data) { console.log(data);}, "json");
+    },
+
+    eventListener: function () {
+      let self = this;
+      if ((typeof jQuery === 'undefined') || (parseFloat(jQuery.fn.jquery) < 1.7)) {
+        self.loadScript('//ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js', function () {
+          var jQuery311 = jQuery.noConflict(true);
+          self.jq = jQuery311;
+          treeappPlugin(jQuery311);
+        });
+      } else {
+        self.jq = jQuery;
+        treeappPlugin(jQuery);
+      }
+    }
+  };
+
+  global.init();
+})();
